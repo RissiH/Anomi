e5679f58a61fc299ecd16ab4f3424f59e3597b5c
Rissi <nyxymproxy76@gmail.com>
2021-03-26 19:35:26 +0000

    Capitalist scum

---
+++ Durkheim/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Durkheim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marxism or Fuctionalism, please write in lower case.");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "functionalism")
+                {
+                    Console.WriteLine("What are you? Gay?");
+
+                }
+                else if (input.ToLower() == "marxism")
+                {
+                    Console.WriteLine("Nice, me too!");
+                }
+                else if (input.ToLower() == "communism") //I need to pee
+                {
+                    Console.WriteLine("Good Evening, Comrade"); //this also does a thing
+                }
+                else
+                {
+                    Console.WriteLine("Answer the question potential capitalist."); //This does a thing
+                }
+            }
+        }
+            
+    }
+}
