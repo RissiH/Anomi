cc69d25644cf671c5ade600d476b42bd02d1c6c1
Rissi <nyxymproxy76@gmail.com>
2021-03-26 18:50:50 +0000

    Assholes, yes plural

---
+++ Anomi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Anomi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lube my assholes with mustard");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "yes")
+                {
+                    Console.WriteLine("Why not try mayonnaise");
+
+                }
+                else if (input.ToLower() == "no")
+                {
+                    Console.WriteLine("Then what the fuck are you doing here then?!");
+                }
+                else
+                {
+                    Console.WriteLine("The fuck you chattting");
+                }
+               
+            }
+           
+        }
+    }
+}
